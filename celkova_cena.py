88b2fa5eaf3aa0c9f3d7672c16537dade6ba0c9a
Oliinka <olga.titzenthaler@gmail.com>
2023-09-27 20:58:23 +0200

    cena

---
+++ celkova_cena.py
@@ -0,0 +1,9 @@
+print("Vitejte v kurzu Pythonu") # řetezec, string, str
+
+nazev_hry = "Romeo a Julie"
+cena_listku = 150
+pocet_listku = 5
+
+celkova_cena = cena_listku * pocet_listku
+
+print(f"Celkova cena je {celkova_cena}")
