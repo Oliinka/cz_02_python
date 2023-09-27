6f4ac336b5ea14843c84cf64e17428c55846385c
Oliinka <olga.titzenthaler@gmail.com>
2023-09-27 22:33:13 +0200

    a

---
+++ indexovani.py
@@ -0,0 +1,10 @@
+#BACHA nebezpeci urazu pri indexovani (Extremne navic procviceni) :exclamation: :exploding_head:
+#Specialne mindblowing struktura na ktery si muzete zkusit indexovani seznamu a slovniku (stanovte si nejakou hodnotu a napiste do threadu jako odpoved jak se k ni doindexovat, napriklad chci 'hello')
+#Poznamka: seznam nemuze byt klic ve slovniku - je to schvalne, ale o tom si muzeme popovidat nekdy jindy :smile:
+
+mindblowing = {
+    'A': {'nashle': 'bye', 'dobry vecer': 'good evening'},
+    'B': [['ahoj', 'hello'],['dobre rano', 'good morning']],
+    'C': {'c' : {'cecka': ['sem nejlepsi']}}
+}
+
